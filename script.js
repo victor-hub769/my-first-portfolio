5d31cb4928da24ab8df4042768c49ba9cd32fb45
Victor <victormuriithi996@gmail.com>
2024-03-18 04:37:15 +0300

    Event listeners wouldnt work, fixing the issue

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const grandparent = document.querySelector(".grandparent");
-const parent = document.querySelector(".parent");
-const child = document.querySelector(".child");
+document.addEventListener("DOMContentLoaded", function() {
+    const grandparent = document.querySelector(".grandparent");
+    const parent = document.querySelector(".parent");
+    const child = document.querySelector(".child");
 
-grandparent.addEventListener("click", (e) => {
-  console.log(e);
+    grandparent.addEventListener("click", function(e) {
+        console.log("Clicked grandparent", e);
+    });
+
+    parent.addEventListener("click", function(e) {
+        console.log("Clicked parent", e);
+    });
+
+    child.addEventListener("click", function(e) {
+        console.log("Clicked child", e);
+    });
 });
